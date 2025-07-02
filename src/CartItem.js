8637c98a3c96e8099fe6d9c2f4280cf748118d39
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-02 10:39:37 +0300

    Modify CartItem class

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,23 @@
 class CartItem {
   constructor(product, selected_choices, quantity) {
     this.product = product;
-    this.selected_choices = selected_choices;
+    this.selected_choices = selected_choices; // A list of Choice objects
     this.quantity = quantity;
+    this.totalPrice = calculateTotalPrice();
   }
 
-  get totalPrice() {
-    let singlePrice = this.product.price;
+  calculateTotalPrice() {
+    // (Base price + Extras) * Quantity
+    return (this.product.price + this.calculateExtraPrice) * this.quantity;
+  }
+
+  // Private method
+  calculateExtraPrice() {
     if (this.selected_choices) {
       for (const choice of this.selected_choices) {
-        singlePrice += choice.price;
+        singlePrice += choice.price; // Get the price of each choice
       }
     }
-    return singlePrice * this.quantity;
   }
 }
 
