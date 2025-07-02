6dd6fea7c86b075231704df0e4289fd18a08dba1
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-02 11:01:46 +0300

    Remove unnecessary import

--- src/Meal.js
+++ src/Meal.js
@@ -1,10 +1,8 @@
-import Options from './Options.js';
-
 class Meal {
     constructor(name, description, options, price) {
         this.name = name;
         this.description = description;
-        this.options = options; // List of options for the meal, such as condiments list.
+        this.options = options; // List of Option classes for the meal, such as condiments list.
         this.price = price; // Base price
     }
 
