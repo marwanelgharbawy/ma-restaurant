625d287c1f9921d774a0aab4da8b4eb44f976fd3
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-02 11:23:47 +0300

    Implement the Cart class

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,31 @@
 class Cart {
 
     constructor() {
-        this.items = []; // Array to hold items in the cart
+        this.cartItems = []; // Empty array to hold items in the cart
     }
 
     addItem(item) {
-        this.items.push(item);
+        this.cartItems.push(item); // Add CartItem object to the Cart object
     }
+
+    // TODO: If item already exists, increase count instead (must match condiments)
+    // Idea: Create an ID for each item containing product name/ID and selected choices 
+
+    removeItem(index) {
+        if (index >= 0 && index < this.cartItems.length) {
+            this.cartItems.splice(index, 1);
+        }
+    }
+
+    get totalPrice() {
+        let total = 0;
+        for (const item of this.cartItems) {
+            total += item.totalPrice;
+        }
+        return total;
+    }
+
+    // TODO: Apply discounts
 }
 
 export default Cart;
