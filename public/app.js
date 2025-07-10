9684fd21d15b9afac6286d73f825203a6c8aa4cf
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-10 13:05:15 +0300

    Let classes handle cart logic instead

--- public/app.js
+++ public/app.js
@@ -14,6 +14,8 @@ const menuContainer = document.getElementById('menu');
 const cartContainer = document.getElementById('cart');
 const priceElement = document.getElementById('total');
 
+const cart = new Cart();
+
 // Populate the menu->Section with the pizza category
 for (const meal of pizzaCategory.meals) {
     // Create a div which contains meal name, description and price
@@ -22,22 +24,40 @@ for (const meal of pizzaCategory.meals) {
     menuContainer.appendChild(mealElement);
 
     mealElement.onclick = () => {
-        addToCart(meal);
+        const cartItem = new CartItem(meal, [], 1);
+        cart.addItem(cartItem);
+        renderCart();
     }
 }
 
-function addToCart(meal) {
-    const cartItem = new CartItem(meal, [], 1);
+function renderCart() {
+    // Clear the cart container
+    cartContainer.innerHTML = '';
 
-    // Add the item element to the cart container
-    const cartItemElement = document.createElement('div');
-    cartItemElement.textContent = `${meal.name} - $${meal.price} x ${cartItem.quantity}`;
-    cartContainer.appendChild(cartItemElement);
-
-    let mealPrice = meal.price * cartItem.quantity;
-    let currentPrice = parseFloat(priceElement.textContent.replace('$', '')) || 0;
-    priceElement.textContent = `$${(currentPrice + mealPrice).toFixed(2)}`;
+    if (cart.cartItems == 0) {
+        cartContainer.textContent = "Your cart is empty.";
+    } else {
+        for (const item of cart.items) {
+            const cartItemElement = document.createElement('div');
+            cartItemElement.textContent = `${item.meal.name} - $${item.meal.price} x ${item.quantity}`;
+            cartContainer.appendChild(cartItemElement);
+        }
+    }
 }
+
+// We don't need that much DOM manipulation, the Cart and CartItem classes handle everything
+// function addToCart(meal) {
+//     const cartItem = new CartItem(meal, [], 1);
+
+//     // Add the item element to the cart container
+//     const cartItemElement = document.createElement('div');
+//     cartItemElement.textContent = `${meal.name} - $${meal.price} x ${cartItem.quantity}`;
+//     cartContainer.appendChild(cartItemElement);
+
+//     let mealPrice = meal.price * cartItem.quantity;
+//     let currentPrice = parseFloat(priceElement.textContent.replace('$', '')) || 0;
+//     priceElement.textContent = `$${(currentPrice + mealPrice).toFixed(2)}`;
+// }
 
 // displayCart(cart) {
 //     document.createElement('div');
