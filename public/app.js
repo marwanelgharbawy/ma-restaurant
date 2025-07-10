6f6c59f8d8ea4dcf23da2dcf4eafca664b30eea0
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-10 12:10:21 +0300

    Create a functional cart

--- public/app.js
+++ public/app.js
@@ -10,6 +10,41 @@ import { pizzaCategory } from './models/Test.js';
 console.log("It works again!");
 
 // DOM manipulation to display the menu and cart
-const menu = document.getElementById('menu');
-const cart = document.getElementById('cart');
+const menuContainer = document.getElementById('menu');
+const cartContainer = document.getElementById('cart');
+const priceElement = document.getElementById('total');
 
+// Populate the menu->Section with the pizza category
+for (const meal of pizzaCategory.meals) {
+    // Create a div which contains meal name, description and price
+    const mealElement = document.createElement('li');
+    mealElement.textContent = `${meal.name}: ${meal.description} - $${meal.price}`;
+    menuContainer.appendChild(mealElement);
+
+    mealElement.onclick = () => {
+        addToCart(meal);
+    }
+}
+
+function addToCart(meal) {
+    const cartItem = new CartItem(meal, [], 1);
+
+    // Add the item element to the cart container
+    const cartItemElement = document.createElement('div');
+    cartItemElement.textContent = `${meal.name} - $${meal.price} x ${cartItem.quantity}`;
+    cartContainer.appendChild(cartItemElement);
+
+    let mealPrice = meal.price * cartItem.quantity;
+    let currentPrice = parseFloat(priceElement.textContent.replace('$', '')) || 0;
+    priceElement.textContent = `$${(currentPrice + mealPrice).toFixed(2)}`;
+}
+
+// displayCart(cart) {
+//     document.createElement('div');
+//     if (cart.items.length == 0) {
+//         cartDiv.textContent = "The cart is empty.";
+//         return;
+//     }
+
+//     console.log(`Total: $${cart.getTotal()}`);
+// }
